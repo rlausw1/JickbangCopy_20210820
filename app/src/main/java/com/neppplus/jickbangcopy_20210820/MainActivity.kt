33b9d004448688d8c7180bac88246e8ed6e25163
김현지 <guswlhjk@naver.com>
2021-09-26 18:42:30 +0900

    방 데이터를 통째로 넘기려면 => 에러 발생

--- app/src/main/java/com/neppplus/jickbangcopy_20210820/MainActivity.kt
+++ app/src/main/java/com/neppplus/jickbangcopy_20210820/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.neppplus.jickbangcopy_20210820
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.neppplus.jickbangcopy_20210820.adapters.RoomAdapter
@@ -29,5 +30,19 @@ class MainActivity : AppCompatActivity() {
 
         mRoomAdapter = RoomAdapter( this, R.layout.room_list_item, mRoomList )
         roomListView.adapter = mRoomAdapter
+
+        roomListView.setOnItemClickListener { adapterView, view, position, l ->
+
+            val clickedRoom = mRoomList[ position ]
+
+            val myIntent = Intent(this, ViewRoomDeatailActivity::class.java)
+
+            myIntent.putExtra("roomData", clickedRoom)
+
+            startActivity(myIntent)
+
+        }
+
+
     }
 }
