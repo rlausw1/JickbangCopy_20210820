a70ae0d651a37ac6cd6e54890e6e9c454be47159
김현지 <guswlhjk@naver.com>
2021-09-26 18:44:32 +0900

    Serializable활용 -> 방 데이터 통째로 첨부 / 받아주기

--- app/src/main/java/com/neppplus/jickbangcopy_20210820/ViewRoomDeatailActivity.kt
+++ app/src/main/java/com/neppplus/jickbangcopy_20210820/ViewRoomDeatailActivity.kt
@@ -2,10 +2,13 @@ package com.neppplus.jickbangcopy_20210820
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.neppplus.jickbangcopy_20210820.datas.RoomData
 
 class ViewRoomDeatailActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_view_room_deatail)
+
+        val roomData = intent.getSerializableExtra("roomData") as RoomData
     }
 }
