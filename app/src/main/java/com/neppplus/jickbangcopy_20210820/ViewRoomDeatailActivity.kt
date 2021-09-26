c964305f1d0a66b09c815153422dc7bfa7e73caa
김현지 <guswlhjk@naver.com>
2021-09-26 18:45:50 +0900

    기존 클래스 기능 활용 => 방 상세 정보 보여주기

--- app/src/main/java/com/neppplus/jickbangcopy_20210820/ViewRoomDeatailActivity.kt
+++ app/src/main/java/com/neppplus/jickbangcopy_20210820/ViewRoomDeatailActivity.kt
@@ -3,6 +3,7 @@ package com.neppplus.jickbangcopy_20210820
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.neppplus.jickbangcopy_20210820.datas.RoomData
+import kotlinx.android.synthetic.main.activity_view_room_deatail.*
 
 class ViewRoomDeatailActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -10,5 +11,12 @@ class ViewRoomDeatailActivity : AppCompatActivity() {
         setContentView(R.layout.activity_view_room_deatail)
 
         val roomData = intent.getSerializableExtra("roomData") as RoomData
+
+        priceTxt.text = roomData.getFormattedPrice()
+        descriptionTxt.text = roomData.description
+
+        addressTxt.text = roomData.address
+        floorTxt.text = roomData.getFormattedFloor()
+
     }
 }
